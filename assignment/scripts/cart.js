d5c0e78e41c3f689225f5a6a0fadf71b44161991
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-10-13 11:18:24 -0500

    accidentally saved in the wrong place. Updated and correct now.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -6,23 +6,22 @@ let basket = [];
 item = ('');
 const maxItems = 5;
 
-   function isFull(){
-    if (basket.length < maxItems){
-        return false;
-    } else {
-        return true;
-    }
+function isFull(){
+  if (basket.length < maxItems){
+      return false;
+  } else {
+      return true;
+  }
 }
 
 function addItem(item){
-    if (isFull() != true){
-      basket.push(item);
-      return true;
-    } else {
-      console.log('The basket is full');
-      return false;
-    }
+  if (isFull() != true){
+    basket.push(item);
+    return true;
+  } else {
+    return false;
   }
+}
 
 console.log(`Basket is ${basket}`);
 console.log('Adding cup (expect true)', addItem(' cup'));
@@ -30,22 +29,23 @@ console.log('Adding bowl (expect true)', addItem(' bowl'));
 console.log('Adding spoon (expect true)',addItem(' spoon'));
 console.log('Adding knife (expect true)',addItem(' knife'));
 console.log('Adding fork (expect true)',addItem(' fork'));
-console.log('Adding plate (expect true)',addItem(' plate'));
+console.log('Adding plate (expect false)',addItem(' plate'))
+console.log('The basket is full');
 console.log(`Basket is now ${basket}`);
 
 
 function listItems(){
-    basket.forEach(function(item){
-    console.log(item);
-    })
+  const lister = basket.values();
+ for (const value of lister){
+  console.log(value);
+ }
 }
-console.log(listItems());
+listItems();
+
 
 function empty(){
-    console.log('Full basket: ' + basket);
-    basket.length = 0;
-    console.log('Empty basket: ' + basket);
+  console.log('Full basket: ' + basket);
+  basket.length = 0;
+  console.log('Empty basket: ' + basket);
 }
 empty();
-
-
